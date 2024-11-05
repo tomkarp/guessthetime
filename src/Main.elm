ed1d32cc34d4cf688e58478ae79e935263494ce0
Thomas Karp <karpthomas@gmail.com>
2024-11-05 23:52:36 +0100

    Erste Version des Spiels

--- src/Main.elm
+++ src/Main.elm
@@ -1,27 +1,31 @@
-module Main exposing (main)
+module Main exposing (..)
 
-import Browser
+import Browser exposing (..)
 import Html exposing (..)
-import Html.Events exposing (onClick)
-
-
-type alias Flags =
-    { myFlag : String
-    }
+import Html.Events exposing (..)
+import Random
+import Time
 
 
 type alias Model =
-    { txt : String
+    { sollZeit : Int
+    , istZeit : Int
+    , running : Bool
     }
 
 
 type Msg
-    = ButtonClick
+    = Start
+    | Stop
+    | SollZeit Int
+    | Tick Time.Posix
 
 
-init : Flags -> ( Model, Cmd Msg )
-init flags =
-    ( { txt = flags.myFlag 
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( { sollZeit = 15
+      , istZeit = 0
+      , running = False
       }
     , Cmd.none
     )
@@ -30,29 +34,52 @@ init flags =
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        ButtonClick ->
-            ( { model | txt = "Button clicked" }
-            , Cmd.none
+        Start ->
+            ( { model | running = True, istZeit = 0 }
+            , Random.generate SollZeit (Random.int 10 20)
             )
+
+        Stop ->
+            ( { model | running = False }, Cmd.none )
+
+        SollZeit zeit ->
+            ( { model | sollZeit = zeit }, Cmd.none )
+
+        Tick _ ->
+            ( { model | istZeit = model.istZeit + 1 }, Cmd.none )
 
 
 view : Model -> Html Msg
 view model =
     div []
-        [ button [ onClick ButtonClick ] [ text "Click me" ]
-        , div [] [ text model.txt ]
+        [ if model.running then
+            button [ onClick Stop ] [ text "Stop" ]
+
+          else
+            button [ onClick Start ] [ text "Start" ]
+        , div []
+            [ if model.running then
+                text ("Stoppe nach " ++ String.fromInt model.sollZeit ++ " Sekunden")
+
+              else
+                text (String.fromInt model.istZeit)
+            ]
         ]
 
 
 subscriptions : Model -> Sub Msg
-subscriptions _ =
-    Sub.none
+subscriptions model =
+    if model.running then
+        Time.every 1000 Tick
+
+    else
+        Sub.none
 
 
 main =
     Browser.element
         { init = init
-        , view = view
         , update = update
+        , view = view
         , subscriptions = subscriptions
         }
